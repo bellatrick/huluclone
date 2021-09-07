64e92c39c0f2aa877234d32af32264ee18556a08
Busayo Samuel <busayosamuel2016@gmail.com>
2021-09-07 15:12:49 +0100

    added runtime configuration

--- next.config.js
+++ next.config.js
@@ -2,5 +2,14 @@ module.exports = {
   reactStrictMode: true,
   images:{
     domains:['links.papareact.com','image.tmdb.org','press.hulu.com']
-  }
+  },
+  serverRuntimeConfig: {
+    // Will only be available on the server side
+    mySecret: 'secret',
+    secondSecret: process.env.API_KEY, // Pass through env variables
+  },
+  publicRuntimeConfig: {
+    // Will be available on both server and client
+    staticFolder: '/static',
+  },
 }
--- utils/requests.js
+++ utils/requests.js
@@ -2,31 +2,31 @@ const API_KEY = process.env.API_KEY;
 const requests = {
   fetchTrending: {
     title: "Trending",
-    url: `trending/all/week?api_key=${API_KEY}&language=en-US`,
+    url: `trending/all/week?api_key=79ce7bd19195d04744565f4aeb59db61&language=en-US`,
   },
   fetchToprated: {
     title: "Top Rated",
-    url: `movie/top_rated?api_key=${API_KEY}&language=en-US`,
+    url: `movie/top_rated?api_key=79ce7bd19195d04744565f4aeb59db61&language=en-US`,
   },
   fetchAction: {
     title: "Action",
-    url: `discover/movie?api_key=${API_KEY}&with_genres=28`,
+    url: `discover/movie?api_key=79ce7bd19195d04744565f4aeb59db61&with_genres=28`,
   },
   fetchComedy: {
     title: "Comedy",
-    url: `discover/movie?api_key=${API_KEY}&with_genres=35`,
+    url: `discover/movie?api_key=79ce7bd19195d04744565f4aeb59db61&with_genres=35`,
   },
   fetchHorror: {
     title: "Horror",
-    url: `discover/movie?api_key=${API_KEY}&with_genres=27`,
+    url: `discover/movie?api_key=79ce7bd19195d04744565f4aeb59db61&with_genres=27`,
   },
   fetchMystery: {
     title: "Mystery",
-    url: `discover/movie?api_key=${API_KEY}&with_genres=9648`,
+    url: `discover/movie?api_key=79ce7bd19195d04744565f4aeb59db61&with_genres=9648`,
   },
   fetchRomance: {
     title: "Romance",
-    url: `discover/movie?api_key=${API_KEY}&with_genres=10749`,
+    url: `discover/movie?api_key=79ce7bd19195d04744565f4aeb59db61&with_genres=10749`,
   },
 };
 export default requests;
